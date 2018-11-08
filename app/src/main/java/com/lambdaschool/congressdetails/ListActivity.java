b361d3b42b6a699b4e7bb2b935864cdec466f744
Chance Payne <chance@lambdaschool.com>
2018-11-08 15:41:11 -0700

    add line to import api access layer in gradle. add example line in listactivity

--- app/src/main/java/com/lambdaschool/congressdetails/ListActivity.java
+++ app/src/main/java/com/lambdaschool/congressdetails/ListActivity.java
@@ -3,11 +3,16 @@ package com.lambdaschool.congressdetails;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.lambdaschool.congressdataapiaccess.CongressDao;
+
+
 public class ListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        CongressDao.getAllMembers();
     }
 }
